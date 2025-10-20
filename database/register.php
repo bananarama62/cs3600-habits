6be33ffd65fb1a5be29c6679c849fdbcf6b29faa
Josh Gillum <gill2092@vandals.uidaho.edu>
2025-10-20 15:41:49 -0700

    Applied website theming and added indicator for when user is already logged in

--- database/register.php
+++ database/register.php
@@ -33,24 +33,109 @@ if($_SERVER["REQUEST_METHOD"] == "POST") {
 ?>
 
 <!DOCTYPE html>
-<html lang="en">
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]>      <html class="no-js"> <!--<![endif]-->
+<html>
   <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Cookies</title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="../CSS/style.css">
+    <script src="../JS/title.js"></script>
   </head>
   <body>
-    <form method="post" class="form-control">
-      <div>
-        <label for="username">User Name:</label>
-        <input type="text" name="username" id="username" class="form-control" required>
+    <!--[if lt IE 7]>
+      <p class="browsehappy">You are using an <strong>outdated</strong> browser. Please <a href="#">upgrade your browser</a> to improve your experience.</p>
+    <![endif]-->
+    <div>
+      <div class="navigation-head">
+        <div class="site-logo">
+          <a href="../index.php">
+            <h1>Habits</h1>
+          </a>
+        </div>
+        <ul class="navigation-menu">
+          <?php
+          session_start();
+          // Check if user is logged in, if not redirect to login page
+          if (!isset($_SESSION['username'])) {
+            echo '<a href="./login.php" class="menu-item">';
+              echo '<li class="underline-hover-effect">Login</li>';
+            echo '</a>';
+            echo '<a href="./register.php" class="menu-item active">';
+              echo '<li class="underline-hover-effect">Register</li>';
+            echo '</a>';
+          } else {
+            echo '<a href="../habits.php" class="menu-item">';
+              echo '<li class="underline-hover-effect">Habits</li>';
+            echo '</a>';
+            echo '<a href="./logout.php" class="menu-item">';
+              echo '<li class="underline-hover-effect">Logout</li>';
+            echo '</a>';
+          }
+          ?>
+        </ul>
       </div>
-      <div>
-        <label for="password">Password:</label>
-        <input type="text" name="password" id="password" class="form-control" required>
-      </div>
-      <div>
-        <button type="submit" name="register">Submit</button>
-      </div>
-    </form>
+      <hr id="head-rule">
+    </div>
+    <div class="breadcrumbs">
+      <a href="../index.php">home</a>
+      <p>></p>
+      <p>register</p>
+    </div>
+    <div class="content">
+      <h1>Register</h1>
+      <?php
+      session_start();
+      // Lets user know they are already logged in
+
+      if (!isset($_SESSION['username'])) {
+        echo '<form method="post" class="form-control">';
+          echo '<div>';
+            echo '<label for="username">User Name:</label>';
+            echo '<input type="text" name="username" id="username" class="form-control" required>';
+          echo '</div>';
+          echo '<div>';
+            echo '<label for="password">Password:</label>';
+            echo '<input type="text" name="password" id="password" class="form-control" required>';
+          echo '</div>';
+          echo '<div>';
+            echo '<button type="submit" name="register">Submit</button>';
+          echo '</div>';
+        echo '</form>';
+      } else {
+        echo "<p>";
+          echo "You are already logged in. Would you like to ";
+          echo "<a href='./logout.php'>";
+            echo "logout";
+          echo "</a>";
+          echo "?";
+        echo "</p>";
+        echo "<p>";
+          echo "Go to ";
+          echo "<a href='../habits.php'>";
+            echo "habits dashboard";
+          echo "</a>";
+        echo "</p>";
+      }
+      ?>
+    </div>
+    <script src="" async defer></script>
+    <hr id="foot-rule">
   </body>
+  <footer>
+    <div class="split-items">
+      <p>Last updated: <span>20 October 2025</span></p>
+      <p>Author: Josh Gillum</p>
+    </div>
+    <div class="split-items">
+      <a href="./cookies.html">cookies</a>
+      <a href="./privacy.html">privacy policy</a>
+      <a href="./terms.html">terms and conditions</a>
+    </div>
+  </footer>
 </html>
